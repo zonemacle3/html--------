57ddc947006bb76a7b50b783eb498b60c2cb3661
zonemacle3 <x000414@naver.com>
2023-05-13 20:38:58 +0900

    javascript newbie

---
+++ pratice.js
@@ -0,0 +1,12 @@
+var title1 = "컴퓨터와 it기술의 이해";
+var title2 = "소셜미디어의 이해와 활용 ";
+var title3 = "멀티미디어 배움터 2.0";
+
+document.write("<caption> 베스트셀러 </caption>");
+document.write("<tr>");
+document.write("<th> 순위 </th>")
+document.write("<th> 제목 </th>")
+document.write("</tr>");
+document.write("<tr> <td> 1 </td> <td> " + title1 + "</td> </tr>");
+document.write("<tr> <td> 2 </td> <td> " + title2 + "</td> </tr>");
+document.write("<tr> <td> 3 </td> <td> " + title3 + "</td> </tr>");
