d2c8373c3b41590ae5ac90b386647b5ea351a5e8
zonemacle3 <x000414@naver.com>
2023-05-14 21:07:31 +0900

    20230514

--- pratice.js
+++ pratice.js
@@ -145,7 +145,7 @@ else {
 document.write("[" + (i+1) + "]" + title + "<br/>"); // 결과창 [숫자] 값만 영향을 줌 
 }
 */
-
+/*
 document.write("<tr>");
 document.write("<td> value </td>");
 
@@ -160,4 +160,124 @@ do{
     fib2 = tmp;
 } while ( fib2<1000)
 document.write("</tr>")
+*/
+/*
+var a = eval("2+3*4+4");
+document.write("evaluation result of a = "+a+"<br/>");
+document.write("value of \"123.45\" = "+ parseInt("123.45") + "<br/>");
+document.write("value of \"123.45\" = "+ parseFloat("123.45") + "<br/>");
+*/
+/*
+function show_alert(){
+    alert("5 seconds passed");
+}
+function show_message(){
+    document.write("3 second passed");
+}
+
+setTimeout(show_alert, 5000);
+setTimeout(show_message, 3000);
+*/
+
+/*
+var today = new Date() // new 연산자를 사용해서 객체를 생성 (date()객체를 사용하기 위해 new date()를 생성함 )
+var y = today.getFullYear(); // 컴퓨터 시계가 제공하는 연도
+var m = today.getMonth()+1; // 메소드의 반환값이 0~11까지라 +1을 더해줌 
+var d = today.getDate(); // 일 
+
+document.write("오늘 날짜 : " + y + "년" + m + "월" + d + "일<br/>");
+
+var start = new Date(); // 
+var t1 = start.getTime();
+
+var sum = 0;
+for(i=0;i<1000000;i++)
+{
+   sum += i;
+}
+
+var end = new Date();
+var t2 = end.getTime();
+
+document.write("1에서 1000000까지 더하는데 걸린 시간: " + (t2-t1) + "ms<br/>");
+document.write("<br/>");
+document.write("sin(60도) = " + Math.sin(Math.PI/3) + "<br/>"); // math객체를 사용하기 위해선 메소드 앞에 math를 붙여야함 
+document.write("ceil(4.3) = " + Math.ceil(4.3) +"<br/>");
+document.write("floor(4.3 = " + Math.floor(4.3) + "<br/>");
+document.write("round(4.3 = " + Math.round(4.3) + "<br/>");
+*/
+
+/* array 배열 
+var book_arr = new Array("멀티미디어 배움터2.0", "생능출판사", "최윤철, 임순범", 25000, 442);
+
+// 배열의 내용 
+// book_arr[0] = "멀티미디어 배움터2.0"
+// book_arr[1] = "생능출판사"
+// ....
+
+var book_arr2 = ["멀티미디어 배움터2.0", "생능출판사", "최윤철, 임순범", 25000, 442];
+
+var arr100 = new Array(100); // 요소 개수가 100인 배열 생성 
+*/
+
+// array배열 2 
+var arr = new Array("one", 2, "3", 4, "five");
+
+arr[6] = 6;
+arr[7] = "seven";
+arr[9] = "3+6";
+// arr = ["one", 2, "3", 4, "five", undefined, 6, "seven", undefined, "3+6"]
+
+document.write("length of array: " + arr.length + "<br/>");
+
+document.write("arr = [");
+for(i=0;i<arr.length;i++) document.write(" "+ arr[i] + "");
+document.write("] <br/> ");
+
+arr.length = 3;
+document.write("length of array : " + arr.length + "<br/>");
+
+document.write("arr = [");
+for(i=0;i<arr.length;i++) document.write(" "+ arr[i] + "");
+document.write("] <br/>");
+
+document.write("arr[" + 100 + "]: " + arr[100] + "<br/>");
+document.write("length of array: " + arr.length + "<br/>");
+
+arr[100] = 100;
+document.write("arr[" + 100 + "]:" + arr[100] + "<br/>");
+document.write("length of array : " + arr.length + "<br/>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
